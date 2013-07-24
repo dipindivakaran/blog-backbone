6c31da7ee946324919e7f0b90018776ed904ee42
dipindp85 <shijup@qburst.com>
2013-07-24 11:21:42 +0530

    Blog application using coffee script and backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9eac4f622dcfe38e99b0bbb3f710dc0e9e8ccfa63d7e987d3d49bcbb22e02638a500099dfadee6b16f543d1bd44850fdbf049e03a01149eb25807e507e199045'
